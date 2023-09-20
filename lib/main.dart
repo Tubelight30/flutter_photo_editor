5fe6b29edc3acd96c9999e322ae397676423e969
Tubelight30 <soumaygupta2004@gmail.com>
2023-09-20 17:38:43 +0530

    theme changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Image Editor App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
+          seedColor: const Color.fromARGB(212, 180, 127, 227),
         ),
         useMaterial3: true,
       ),
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Uint8List? originalImageData;
   Uint8List? imageData;
-  int flag = 0;
+  int imageSelected = 0;
 
   @override
   void initState() {
@@ -57,8 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     final originalImage = img.decodeImage(imageBytes);
     if (originalImage == null) return null;
-    final grayscaleImage =
-        img.grayscale(originalImage); // Apply grayscale filter
+    final grayscaleImage = img.sepia(originalImage); // Apply grayscale filter
 
     return Uint8List.fromList(img.encodeJpg(grayscaleImage));
   }
@@ -69,7 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
       final filePath = '${appDocDir!.path}/edited_image.png';
       final file = File(filePath);
       await file.writeAsBytes(editedImageData);
-      //if (mounted)
       showDialogBox(context, 'Image saved successfully to $filePath');
     } on Exception catch (e) {
       showDialogBox(context, "Image not saved: $e");
@@ -92,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
     final imageTemporary = File(image.path);
     setState(
       () {
-        flag = 1;
+        imageSelected = 1;
         originalImageData = imageTemporary.readAsBytesSync();
         imageData = imageTemporary.readAsBytesSync();
       },
@@ -103,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.purple[100],
         title: Text(widget.title),
       ),
       body: Center(
@@ -111,6 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Padding(
             padding: const EdgeInsets.all(40.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 imageData != null
                     ? SizedBox(
@@ -144,11 +143,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      margin: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
                       child: ElevatedButton(
                         onPressed: () async {
                           try {
-                            if (flag != 1) {
+                            if (imageSelected != 1) {
                               throw Exception("Please select an image first");
                             }
                             final editedImage = await applyFilter(imageData);
@@ -163,15 +162,15 @@ class _MyHomePageState extends State<MyHomePage> {
                             showDialogBox(context, "$e");
                           }
                         },
-                        child: const Text("Apply Filter"),
+                        child: const Text("Apply a Filter"),
                       ),
                     ),
                     Container(
-                      margin: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
                       child: ElevatedButton(
                         onPressed: () async {
                           try {
-                            if (flag != 1) {
+                            if (imageSelected != 1) {
                               throw Exception("Please select an image first");
                             }
                             var editedImage = await Navigator.push(
@@ -190,7 +189,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             showDialogBox(context, "$e");
                           }
                         },
-                        child: const Text("Image editor"),
+                        child: const Text("Full Image editor"),
                       ),
                     ),
                   ],
@@ -208,7 +207,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                         onPressed: () async {
                           try {
-                            if (flag != 1) {
+                            if (imageSelected != 1) {
                               throw Exception("Please select an image first");
                             }
                             await saveEditedImage(imageData!);
@@ -224,11 +223,13 @@ class _MyHomePageState extends State<MyHomePage> {
                       margin: const EdgeInsets.all(10),
                       child: ElevatedButton(
                         style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Color.fromARGB(255, 241, 86, 34))),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromARGB(255, 241, 86, 34),
+                          ),
+                        ),
                         onPressed: () {
                           try {
-                            if (flag != 1) {
+                            if (imageSelected != 1) {
                               throw Exception("Please select an image first");
                             }
                             imageData = originalImageData;
